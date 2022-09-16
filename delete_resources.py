03167163e5cf2979b0a08030254d3fdf5203767f
Pat@Maendeleolab <maendeleolab@gmail.com>
2022-09-16 01:54:56 -0400

    corrected format

--- delete_resources.py
+++ delete_resources.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-import os, json
-from build_prefix_list import destroy_prefix_list, erase_prefix_list
+from maendeleolab_lib import *
 
-#maendeleolab_infra=['us-east-1','us-west-2']
-maendeleolab_infra=['us-east-2']
+maendeleolab_infra=[
+        'us-east-1',
+        'us-west-2',
+        ]
 
 for region in maendeleolab_infra:
-	erase_prefix_list(region)
+    erase_prefix_list(region)
 
 # ------------------------ End ---------------------------
